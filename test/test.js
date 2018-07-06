76159a1f452f875fb67062bbdb5e1994366b7492
vikash kumar <krvikash35@gmail.com>
2018-07-07 00:53:06 +0530

    added libc.so just for test

--- test/test.js
+++ test/test.js
@@ -6,7 +6,8 @@ const path = require('path');
 describe('module', function () {
   it('should be funcion that return object with desired methods', function () {
     assert(typeof dlopen === 'function');
-    var lib =  dlopen('libc');
+    const libpath = path.join(__dirname, './libc-2.26.so')
+    var lib =  dlopen(libpath);
     assert(lib instanceof dlopen);
     assert(dlopen.prototype.hasOwnProperty('get'));
     assert(dlopen.prototype.hasOwnProperty('close'));
